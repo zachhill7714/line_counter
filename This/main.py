7fc4d7c10a0390af4c2f8e1dc0f94fafc203f7ec
zachhill7714 <55302876+zachhill7714@users.noreply.github.com>
2023-02-02 02:16:53 -0500

    Initial commit

---
+++ This/main.py
@@ -0,0 +1,62 @@
+import os
+from datetime import date
+
+
+def get_line_count(filepath):
+    file = open(filepath, 'r')
+    line_count = len(file.readlines())
+    return line_count
+
+
+def get_config_data(filepath):
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    language = lines[0]
+    excludes = lines[1].split(' ')
+    filetypes = lines[2].split(' ')
+    return [language, excludes, filetypes]
+
+
+def get_relevant_files(filepath):
+    files = []
+    paths = os.listdir(filepath)
+
+    for path in paths:
+        new_path = os.path.join(filepath, path)
+        if path not in excludes:
+            if os.path.isfile(new_path):
+                tokens = path.split('.')
+                extension = tokens[-1:][0]
+                if extension in filetypes:
+                    files.append(new_path)
+            else:
+                sub_files = get_relevant_files(new_path)
+                for file in sub_files:
+                    files.append(file)
+    return files
+
+
+total_lines = 0
+
+config_data = get_config_data("config.cfg")
+
+language = config_data[0].strip()
+excludes = config_data[1]
+filetypes = config_data[2]
+
+filepaths = get_relevant_files('.')
+
+for filepath in filepaths:
+    total_lines += get_line_count(filepath)
+
+readme = open("readme.md", 'r+')
+identifier = f"### Total lines of {language} source code as of "
+string = f"{identifier}{str(date.today())} = {str(total_lines)} lines"
+readme_lines = readme.readlines()
+chars = 0
+for i in range(len(readme_lines)):
+    if len(readme_lines[i].split(identifier)) > 1:
+        readme.seek(chars)
+    chars += len(readme_lines[i]) + 1
+
+readme.write(string)
---
+++ This/readme.md
@@ -0,0 +1,7 @@
+test test
+
+
+
+### Total lines of Python source code as of 2023-02-02 = 62 lines
+test test
+the cat
